c5a600446851a4d8e836463e7db597e6db4bc6be
luismoura03 <gui.batidao3@gmail.com>
2025-01-08 14:27:00 -0300

    🛠  Treatment created for non existent ID

--- resolvers/books/query/get.js
+++ resolvers/books/query/get.js
@@ -2,11 +2,10 @@ const db = require("../../../src/db/index");
 
 module.exports = {
   book: async (_, { id }) => {
-    const book = await db("books as b")
-      .join("authors as a", "b.author_id", "a.id")
-      .where("b.id", id)
-      .select("b.id", "b.title", "b.author_id", "a.name as author_name")
-      .first();
+    const book = await db("books").where({ id }).first();
+    if (!book) {
+      throw new Error(`Book not found: ID ${id}`);
+    }
     return book;
   },
 };
